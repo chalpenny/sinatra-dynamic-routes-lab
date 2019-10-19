f5acdd54ac0e355820d9df65e144d3b9b7f0cf89
chalpenny <cp.halpenny@gmailcom>
2019-10-19 11:06:25 -0700

    Done.

--- app.rb
+++ app.rb
@@ -10,15 +10,15 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @number = params[:number.to_i]
-    "#{Math.sqrt(@number)}"
+    "#{Math.sqrt(@number).to_s}"
   end
 
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
-    @phrase << (" ")
+    #@phrase << (" ")
     @number = params[:number].to_i
-    full_phrase = @phrase * @number
-    puts "#{full_phrase.strip}"
+    #full_phrase = @phrase * @number
+    full_phrase.each {|p| puts "#{@phrase}" * @number}
   end
 
     get '/say/:word1/:word2/:word3/:word4/:word5' do
